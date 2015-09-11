cedf1ee05d36cfbe137d366c441fed140d0f9654
Karthik <karthik.kasala@gmail.com>
2015-09-11 09:33:27 -0400

    classes initially defined

---
+++ movie_lib.py
@@ -0,0 +1,30 @@
+class User:
+    """docstring for """
+    def __init__(self, user_id):
+        self.id = user_id
+
+
+class Movie:
+    """docstring for """
+    def __init__(self, movie_id, title):
+        self.id = movie_id
+        self.title = title
+        #self.genre = genre
+
+class Rating:
+    """docstring for """
+    def __init__(self, user_id, movie_id, stars):
+        #stars represents rating
+        self.id = user_id
+        self.movie_id = movie_id
+        self.stars = stars
+        
+
+
+
+"""Return all ratings for a specific movie ID"""
+def get_movie_ratings():
+
+    return stars for movie_id in range()
+
+"""
