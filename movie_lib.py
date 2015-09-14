fc89f12f76f4214ff1d5edbd6a19110c1b79121a
Karthik <karthik.kasala@gmail.com>
2015-09-14 07:31:07 -0400

    added data files

--- movie_lib.py
+++ movie_lib.py
@@ -1,30 +1,67 @@
+import csv
+
+all_movies = {} # Movie objects go in here
+all_users = {} #  User objects go in here
+
 class User:
-    """docstring for """
+
     def __init__(self, user_id):
         self.id = user_id
+        all_users[self.id] = self
+        self.ratings = {}
 
 
 class Movie:
-    """docstring for """
-    def __init__(self, movie_id, title):
+
+    def __init__(self, movie_id, title, **kwargs):
         self.id = movie_id
         self.title = title
-        #self.genre = genre
+        self.ratings = {}
+        all_movies[self.id] = self #when a Movie object is created, will go in the dictionary all_movies
+        #self.ratings = {} # key: user_id, #value: Rating object
+
+
+    def add_rating(self, rating):
+        self.ratings[rating.user_id] = rating
+
+
+    def get_ratings(self):
+        return self.ratings.values() # look at ipython in terminal, pretty cool
+
+    def __str__(self):
+        return 'Movie(movie_id={}, title={})'.format(self.movie_id, repr(self.title))
+
+    def ___repr__(self):
+        return self.__str__()
 
 class Rating:
-    """docstring for """
     def __init__(self, user_id, movie_id, stars):
         #stars represents rating
-        self.id = user_id
+        self.user_id = user_id
         self.movie_id = movie_id
         self.stars = stars
-        
+
+        all_movies[self.movie_id].add_rating(self)
+        #all_users[self.user_id].add_rating(self)
+
+    def __str__(self):
+        return "Rating(user_id ={}, movie_id = {}, rating = {} stars).".format(self.user_id, self.movie_id, self.stars)
+    def __repr__(self):
+        return self.__str__()
 
 
-
-"""Return all ratings for a specific movie ID"""
-def get_movie_ratings():
-
-    return stars for movie_id in range()
-
-"""
+def movie_list():
+    with open('u.item', encoding='latin_1') as f:
+        reader = csv.DictReader(f, fieldnames=('movie_id', 'movie_title', " ", " ", "something else"), delimiter = '|')
+        for row in reader:
+            Movie(row('movie_id'), row('movie_title'))
+def user_list():
+    with open('u.user') as f:
+        reader = csv.DictReader(f, fieldnames='user_id', delimiter = '|')
+        for row in reader:
+            User(row('user_id'))
+def ratings_list():
+    with open('u.data') as f:
+        reader = csv.DictReader(f, fieldnames=('user_id', 'movie_id', 'rating'), delimiter = 'space')
+        for row in reader:
+            Rating(row('user_id'), row('movie_id'), row('rating'))
---
+++ movie_lib_test.py
@@ -0,0 +1,41 @@
+from movie_lib import *
+
+ebert = User(5)
+siskel = User(12)
+movie1 = Movie(3, "Toy Story")
+movie2 = Movie(9, "Pretty Woman")
+
+print(all_movies)
+
+rating1 = Rating(siskel.id, movie1.id, 4)
+rating2 = Rating(ebert.id, movie2.id, 1)
+rating3 = Rating(siskel.id, movie2.id, 3)
+rating4 = Rating(ebert.id, movie1.id, 1)
+
+def test_user_creation():
+    assert ebert.id ==5
+    assert siskel.id ==12
+
+def test_movie_creation():
+    assert movie1.id == 3
+    assert movie1.title == 'Toy Story'
+    assert movie2.id == 9
+    assert movie2.title == 'Pretty Woman'
+
+def test_rating_creation():
+    assert rating1.user_id == rating3.user_id == siskel.id
+    assert rating2.user_id == rating4.user_id == ebert.id
+    assert rating1.movie_id == rating4.movie_id == movie1.id
+    assert rating2.movie_id == rating3.movie_id == movie2.id
+    assert rating1.stars == 4
+    assert rating2.stars == 1
+    assert rating3.stars == 3
+    assert rating4.stars == 1
+
+def test_find_ratings_for_movie():
+
+    #return a list of Rating objects
+    toy_story_ratings =all_movies[movie1.id].get_ratings()
+    print (len(toy_story_ratings))
+    print(toy_story_ratings)
+    assert len(toy_story_ratings) == 2
